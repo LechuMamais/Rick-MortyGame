670ed345fdcc11e45337e499e53b38ea6e2b8062
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-05-27 16:04:02 +0200

    Upgrading carousel

--- src/components/CharactersCarouselContainer/CharactersCarouselContainer.jsx
+++ src/components/CharactersCarouselContainer/CharactersCarouselContainer.jsx
@@ -3,101 +3,6 @@ import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
-const handleDragStart = (e) => e.preventDefault();
-
-const items = [
-  <div
-    className="character-guess-card"
-    onDragStart={handleDragStart}
-    role="presentation"
-  >
-    <img
-      src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-      alt="Rick Sanchez"
-    />
-    <div className="character-guess-card-text-container">
-      <div className="character-guess-card-name-container">
-        <h2>Rick Sanchez</h2>
-      </div>
-      <div className="character-guess-card-score-container">
-        <h2>Best Score: 0</h2>
-      </div>
-    </div>
-  </div>,
-  <div
-    className="character-guess-card"
-    onDragStart={handleDragStart}
-    role="presentation"
-  >
-    <img
-      src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-      alt="Morty Smith"
-    />
-    <div className="character-guess-card-text-container">
-      <div className="character-guess-card-name-container">
-        <h2>Morty Smith</h2>
-      </div>
-      <div className="character-guess-card-score-container">
-        <h2>Best Score: 0</h2>
-      </div>
-    </div>
-  </div>,
-  <div
-    className="character-guess-card"
-    onDragStart={handleDragStart}
-    role="presentation"
-  >
-    <img
-      src="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-      alt="Summer Smith"
-    />
-    <div className="character-guess-card-text-container">
-      <div className="character-guess-card-name-container">
-        <h2>Summer Smith</h2>
-      </div>
-      <div className="character-guess-card-score-container">
-        <h2>Best Score: 0</h2>
-      </div>
-    </div>
-  </div>,
-  <div
-    className="character-guess-card"
-    onDragStart={handleDragStart}
-    role="presentation"
-  >
-    <img
-      src="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
-      alt="Beth Smith"
-    />
-    <div className="character-guess-card-text-container">
-      <div className="character-guess-card-name-container">
-        <h2>Beth Smith</h2>
-      </div>
-      <div className="character-guess-card-score-container">
-        <h2>Best Score: 0</h2>
-      </div>
-    </div>
-  </div>,
-  <div
-    className="character-guess-card"
-    onDragStart={handleDragStart}
-    role="presentation"
-  >
-    <img
-      src="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
-      alt="Jerry Smith"
-    />
-    <div className="character-guess-card-text-container">
-      <div className="character-guess-card-name-container">
-        <h2>Jerry Smith</h2>
-      </div>
-      <div className="character-guess-card-score-container">
-        <h2>Best Score: 0</h2>
-      </div>
-    </div>
-  </div>,
-];
-
 const responsive = {
     0: {items: 3}
 }
@@ -110,6 +15,8 @@ const CharactersCarouselContainer = ({ mainCharactersItems }) => {
           responsive={responsive} 
           items={mainCharactersItems} 
           infinite
+          paddingLeft={0}
+          paddingRight={28}
         />
       </div>
     );
